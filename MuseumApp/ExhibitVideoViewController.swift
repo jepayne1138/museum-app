eaa5f37de8f0b7ffb3e76abb35d56872a75970d4
James Payne <jepayne1138@gmail.com>
2015-12-13 16:53:48 -0600

    Add AVPlayer and attempt to play video by when button is pressed

--- MuseumApp/ExhibitVideoViewController.swift
+++ MuseumApp/ExhibitVideoViewController.swift
@@ -7,13 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class ExhibitVideoViewController: ExhibitViewControllerBase {
+
+    @IBOutlet weak var playVideoButton: UIButton!
+    @IBOutlet weak var descriptionTextView: UITextView!
+
+    var localPathURL: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        localPathURL = exhibit!.resource!.localPath
+        if localPathURL! != "" {
+            playVideoButton.enabled = true
+            if let snapshotImage = videoSnapshot(localPathURL!) {
+                playVideoButton.setImage(snapshotImage, forState: UIControlState.Normal)
+            }
+        }
+        else {
+            // Disable the button as the resource is not yet loaded
+            playVideoButton.enabled = false
+        }
+
         // Do any additional setup after loading the view.
+        descriptionTextView.text = exhibit!.text
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +41,17 @@ class ExhibitVideoViewController: ExhibitViewControllerBase {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-    }
-    */
+        let destView = segue.destinationViewController as? AVPlayerViewController
 
+        if let unwrappedPath = localPathURL {
+            let url = NSURL(string: unwrappedPath)
+            destView!.player = AVPlayer(URL: url!)
+        }
+    }
 }
